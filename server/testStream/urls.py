77100da05dd8e1cacf5af99371d47961fc25ef9c
sjbae <sjbae@bitmango.com>
2021-08-21 17:38:19 +0900

    기본적인 서버구조 생성

--- server/testStream/urls.py
+++ server/testStream/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('testStream/', include('testStream.urls')),
-    path('testUnity/', include('testUnity.urls')),
+    path('', views.home),
 ]
